58da3113908dc3bed96d69921873fb1d18e5378c
hayden365 <ysmh100@gmail.com>
2023-05-28 01:20:38 +0900

    stub

--- src/mock/product/test/product_service_no_di.test.js
+++ src/mock/product/test/product_service_no_di.test.js
@@ -1,4 +1,7 @@
 //mock을 남용하는 사례
+//mock -> 구현사항이 없고, 부분적으로 흉내.
+//Stub -> 실제로 구현된코드, 진짜와 대체가능한 코드.
+
 const ProductService = require("../product_service_no_di.js");
 const ProductClient = require("../product_client.js");
 jest.mock("../product_client");
@@ -23,10 +26,5 @@ describe("ProductService", () => {
 		const items = await productService.fetchAvailableItems();
 		expect(items.length).toBe(1);
 		expect(items).toEqual([{ item: "Milk", available: true }]);
-	});
-
-	it("test", async () => {
-		const items = await productService.fetchAvailableItems();
-		expect(fetchItems).toHaveBeenCalledTimes(1);
 	});
 });
